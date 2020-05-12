cb6e2cfcd908ece3983aa93e6cf64eeb40c85654
JonathanDowell <mr.jonathandowell@gmail.com>
2020-05-11 20:03:56 -0500

    user can view posts

---
+++ src/views/UsersShow.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h1>All Posts</h1>
+    <div v-for="post in posts">
+      <h2>{{ post.text }}</h2>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      posts: []
+    };
+  },
+  created: function() {
+    axios.get("/api/users/"+this.$route.params.id).then(response => {
+      console.log(response.data)
+      this.posts = response.data.posts;
+    });
+  },
+  methods: {}
+};
+</script>
+
+
