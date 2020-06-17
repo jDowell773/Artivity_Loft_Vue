b389827c5a1ba524b446e106ece1c0bc2ae9fe42
JonathanDowell <mr.jonathandowell@gmail.com>
2020-06-16 21:29:35 -0500

    update github link

--- src/App.vue
+++ src/App.vue
@@ -94,12 +94,11 @@
 
 <section id="footer">
 <ul class="icons">
-  <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-  <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+  <!-- <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li> -->
+  <!-- <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li> -->
   <li><a href="#" class="icon brands fa-google-plus-g"><span class="label">Google+</span></a></li>
-  <li><a href="#" class="icon brands fa-pinterest"><span class="label">Pinterest</span></a></li>
-  <li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
-
+  <!-- <li><a href="#" class="icon brands fa-pinterest"><span class="label">Pinterest</span></a></li> -->
+  <li><a href="https://github.com/jDowell773" class="icon brands fa-github"><span class="label">Github</span></a></li>
   <!-- <li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li> -->
   <li><a href="https://www.linkedin.com/in/jonathan-dowell773/" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
 </ul>
